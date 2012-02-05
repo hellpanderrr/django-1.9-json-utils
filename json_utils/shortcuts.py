01aa1d0e29f8226691ce4a773bed64dce82208e3
Tom Najdek <tom@doppnet.com>
2012-02-05 15:42:40 +0000

    Fixed shortcut to correctly deal with http requests without HTTP_ACCEPT_ENCODING header

--- json_utils/shortcuts.py
+++ json_utils/shortcuts.py
@@ -1,7 +1,8 @@
 from json_utils.http import JSONResponse
 
 def response_json(request, content):
-	if "application/json" in request.META['HTTP_ACCEPT_ENCODING']:
+
+	if request.META.has_key('HTTP_ACCEPT_ENCODING') and "application/json" in request.META['HTTP_ACCEPT_ENCODING']:
 		mimetype = 'application/json'
 	else:
 		mimetype = 'text/plain'
