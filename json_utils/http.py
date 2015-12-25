1b924374b2c233583220716f7c41ebeed718ffc0
hellpanderrr <hellpanderrr@users.noreply.github.com>
2015-12-25 10:01:42 +0300

    Update http.py

--- json_utils/http.py
+++ json_utils/http.py
@@ -3,7 +3,7 @@ from django import http
 from json_utils import utils
 
 class JSONResponse(http.HttpResponse):
-    def __init__(self, content, mimetype='application/json'):
+    def __init__(self, content, content_type='application/json'):
         # Considered adding some CSRF protection. Decided against it in favour
         # of the Django middleware and other more generic solutions.
         content = utils.to_json(content)
